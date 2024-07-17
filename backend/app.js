3eaffcadc4c09bee50ca1d239e874f987c88b3fd
Sumit Agarwal <sumitagarwal2002@gmail.com>
2024-07-17 16:43:38 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -185,7 +185,7 @@ const app = express();
 const http = require('http').createServer(app); 
 const io = require('socket.io')(http, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://paws4you.netlify.app/",
     methods: ["GET", "POST"]
   }
 });
